a5aa20308041d38729389202625bbf557529ced2
Alexey Lesovsky <lesovsky@gmail.com>
2019-06-08 12:25:20 +0500

    v0.6.2

--- cmd/version.go
+++ cmd/version.go
@@ -12,7 +12,7 @@ const (
 	// ProgramVersion is the version of this program
 	ProgramVersion = "0.6"
 	// ProgramRelease is release number of this program
-	ProgramRelease = "1-devel"
+	ProgramRelease = "2"
 	// ProgramIssuesUrl is the public URL for posting issues, bug reports and asking questions
 	ProgramIssuesUrl = "https://github.com/lesovsky/pgcenter/issues"
 )
