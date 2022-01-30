9f09649274d08e16dc734b23e85e6bdaac7b400b
Lesovsky Aleksey <Lesovsky.Aleksey@wb.ru>
2022-01-30 12:17:55 +0500

    Do not truncate long values of pg_stat_activity.application_name. Fix docs.

--- doc/development.md
+++ doc/development.md
@@ -2,7 +2,7 @@
 
 Download pgcenter-testing docker image, run container and start script which prepares testing environment.
 ```shell
-$ git pull lesovsky/pgcenter-testing:latest
+$ docker pull lesovsky/pgcenter-testing:latest
 $ docker run --rm -p 21995:21995 -p 21996:21996 -p 21910:21910 -p 21911:21911 -p 21912:21912 -p 21913:21913 -p 21914:21914 -ti lesovsky/pgcenter-testing:latest /bin/bash
 # prepare-test-environment.sh
 ```
--- internal/query/activity.go
+++ internal/query/activity.go
@@ -4,7 +4,7 @@ const (
 	// PgStatActivityDefault is the default query for getting stats from pg_stat_activity view.
 	// - regexp_replace() removes extra spaces, tabs and newlines from queries.
 	PgStatActivityDefault = "SELECT pid, host(client_addr) AS cl_addr, client_port AS cl_port, " +
-		"datname, usename, left(application_name, 16) AS appname, backend_type, " +
+		"datname, usename, application_name AS appname, backend_type, " +
 		"wait_event_type AS wait_etype, wait_event, state, " +
 		"date_trunc('seconds', clock_timestamp() - xact_start)::text AS xact_age, " +
 		"date_trunc('seconds', clock_timestamp() - query_start)::text AS query_age, " +
@@ -18,7 +18,7 @@ const (
 	// PgStatActivity96 queries for getting stats from pg_stat_activity view for versions 9.6.*.
 	// - regexp_replace() removes extra spaces, tabs and newlines from queries.
 	PgStatActivity96 = "SELECT pid, host(client_addr) AS cl_addr, client_port AS cl_port, datname, " +
-		"usename, left(application_name, 16) AS appname, wait_event_type AS wait_etype, " +
+		"usename, application_name AS appname, wait_event_type AS wait_etype, " +
 		"wait_event, state, date_trunc('seconds', clock_timestamp() - xact_start)::text AS xact_age, " +
 		"date_trunc('seconds', clock_timestamp() - query_start)::text AS query_age, " +
 		"date_trunc('seconds', clock_timestamp() - state_change)::text AS change_age, " +
@@ -31,7 +31,7 @@ const (
 	// PgStatActivity95 queries activity stats from pg_stat_activity view from versions for 9.5.* and later.
 	// - regexp_replace() removes extra spaces, tabs and newlines from queries.
 	PgStatActivity95 = "SELECT pid, host(client_addr) AS cl_addr, client_port AS cl_port, datname, " +
-		"usename, left(application_name, 16) AS appname, waiting, state, " +
+		"usename, application_name AS appname, waiting, state, " +
 		"date_trunc('seconds', clock_timestamp() - xact_start)::text AS xact_age, " +
 		"date_trunc('seconds', clock_timestamp() - query_start)::text AS query_age, " +
 		"date_trunc('seconds', clock_timestamp() - state_change)::text AS change_age, " +
